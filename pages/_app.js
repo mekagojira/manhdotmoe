414c1a93d15e36649b91da32cd6e281d62dc481a
MDN <hihuhiro@gmail.com>
2022-02-01 15:36:10 +0700

    Init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import 'scss/index.scss';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <NextSeo
+                title="manh.moe"
+                description="Call me MDN"
+                canonical="https://www.manh.moe/"
+                openGraph={{
+                    url: 'https://www.url.ie/a',
+                    title: 'manh.moe',
+                    description: 'Call me MDN',
+                    images: [
+                        {
+                            url: '/san.jpeg',
+                            width: 886,
+                            height: 886,
+                            alt: 'San',
+                            type: 'image/jpeg',
+                        },
+                    ],
+                    site_name: 'manh.moe',
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        fontFamily: {
+            body: ['Poppins'],
+            sans: ['Poppins', 'sans-serif'],
+            mono: ['Courier Prime', 'monospace'],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
